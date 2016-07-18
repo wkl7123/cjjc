2545aa41780bde7d137540327e932184a4b7f576
wkl7123 <wkl7123@gmail.com>
2016-07-18 22:46:37 +0800

    Add files via upload
    发现查询優しい的时候有不对劲的地方，主要修改了cj.py

--- cj.py
+++ cj.py
@@ -5,7 +5,6 @@ import sys
 from termcolor import colored,cprint
 def hj_query(word):
     html=pq('http://dict.hjenglish.com/jp/cj/'+word, headers={'user-agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'})
-    file=open("temp.html",'wr')
     container=html('div.mian_container.main_container')
     string_template='#headword_jp_'
     def my_print(x):
@@ -20,7 +19,11 @@ def hj_query(word):
         hiragana=target('div.mt10 span.trs_jp.bold font').text()
         english=target('div.mt10 span.trs_jp.bold').eq(1).text()
         # english='['+english[1:-1]+']'
-        cprint(kannji+' '+hiragana+' '+english,'magenta')
+        if i==1:
+            cprint('-'*38+' ✂ '+'-'*39,'white')
+        else:
+            cprint('-  -'*20,'white')
+        cprint(chr(64+i)+'  '+kannji+' '+hiragana+' '+english,'magenta')
         main=target('div.word_ext_con.clearfix')
         type_name=main('div.flag.big_type.tip_content_item').html() #May be None
         if type_name!=None:
@@ -37,11 +40,11 @@ def hj_query(word):
                 tatoes=re.sub(r'<br/>','\n',tatoes)
                 tatoes=re.sub(r'<b>' ,'',tatoes)
                 tatoes=re.sub(r'</b>','',tatoes)
-                tatoes=re.sub(r'/','\t/',tatoes)
+                tatoes=re.sub(r'/','\n  ',tatoes)
                 if tatoes[0]=="\n":
                     tatoes=tatoes[1:]
                 print tatoes
+    if i!=1:
         cprint('-'*38+' ✂ '+'-'*39,'white')
-        print ""
 hj_query(sys.argv[1])
 
